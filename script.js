bd4946c659445a60a10305cf9d58f4de8d8a03ae
krishan raaj <143328587+krishanraaj@users.noreply.github.com>
2025-09-21 10:51:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-//your JS code here. If required.
 const container = document.querySelector('.container');
-const SQUARES = 30*30-100;
+  const SQUARES = 30 * 30 - 100;
 
+  function getRandomColor() {
+    const hex =
+      '#' +
+      Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0');
+    return hex;
+  }
 
-function getRandomColor() {
-  // Generates a random color in hex format
-  const hex = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
-  return hex;
-}
+  for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
 
-for (let i = 0; i < SQUARES; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
-
-  square.addEventListener('mouseenter', () => {
-    const color = getRandomColor();
-    square.style.background = color;
-    setTimeout(() => {
-      square.style.background = '#1d1d1d';
-    }, 1000);
-  });
-}
+    square.addEventListener('mouseenter', () => {
+      const color = getRandomColor();
+      square.style.background = color;
+      setTimeout(() => {
+        square.style.background = '#1d1d1d';
+      }, 1000);
+    });
+  }
