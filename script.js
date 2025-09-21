e54014a9c53cf1d3e733c9cc27c283c4d5d0cae0
krishan raaj <143328587+krishanraaj@users.noreply.github.com>
2025-09-21 10:58:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const container = document.querySelector('.container');
-  const SQUARES = 30 * 30 - 100;
+const SQUARES = 30 * 30 - 100;
 
-  function getRandomColor() {
-    const hex =
-      '#' +
-      Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, '0');
-    return hex;
-  }
+function getRandomColor() {
+  return (
+    '#' +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')
+  );
+}
 
-  for (let i = 0; i < SQUARES; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
 
-    square.addEventListener('mouseenter', () => {
-      const color = getRandomColor();
-      square.style.background = color;
-      setTimeout(() => {
-        square.style.background = '#1d1d1d';
-      }, 1000);
-    });
-  }
+  square.addEventListener('mouseenter', () => {
+    const color = getRandomColor();
+    square.style.background = color;
+    setTimeout(() => {
+      square.style.background = '#1d1d1d'; // back to default
+    }, 1000);
+  });
+}
